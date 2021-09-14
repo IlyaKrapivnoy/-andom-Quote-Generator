85a39e5ad0f58e2c60652c1c886d8546127f8216
ilya.krapivnoy <ilya.krapivnoy@gmail.com>
2021-09-14 20:13:05 +0300

    fetching data in the class comp

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-    return <div className='App'></div>;
+class App extends Component {
+    state = {
+        advice: '',
+    };
+
+    componentDidMount() {
+        this.fetchAdvice();
+    }
+
+    fetchAdvice = () => {
+        axios
+            .get('https://api.adviceslip.com/advice')
+            .then((response) => {
+                console.log('hm', response);
+            })
+            .catch((error) => {
+                console.log('hm', error);
+            });
+    };
+
+    render() {
+        return <div className='App'>hi</div>;
+    }
 }
 
 export default App;
