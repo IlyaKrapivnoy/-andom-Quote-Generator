c55c6dedb45e28ccd1bd279bc2c3647bfc03ab1c
IlyaKrapivnoy <ilya.krapivnoy@gmail.com>
2023-03-03 14:25:58 +0100

    add errorMessage

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import axios from "axios";
 
 const App = () => {
   const [advice, setAdvice] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
   useEffect(() => {
     fetchAdvice();
@@ -14,12 +15,14 @@ const App = () => {
       .get("https://api.adviceslip.com/advice")
       .then((response) => {
         const { advice } = response.data.slip;
-        console.log("quote", advice);
 
         setAdvice(advice);
       })
       .catch((error) => {
-        console.log("hm", error);
+        console.error("Error fetching advice:", error);
+
+        setAdvice("");
+        setErrorMessage("Failed to fetch advice. Please try again later.");
       });
   };
 
@@ -27,6 +30,11 @@ const App = () => {
     <div className="app">
       <h1 className="title">Advice update every 5 seconds</h1>
       <div className="card">
+        {errorMessage ? (
+          <p className="error-message">{errorMessage}</p>
+        ) : (
+          <h2 className="heading">{advice}</h2>
+        )}
         <h2 className="heading">{advice}</h2>
         <button className="button" onClick={fetchAdvice}>
           <span>GIVE ME ADVICE!</span>
