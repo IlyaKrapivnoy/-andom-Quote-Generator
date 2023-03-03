21919b310d0090c26a07373688ea1abbe1399bfc
IlyaKrapivnoy <ilya.krapivnoy@gmail.com>
2023-03-03 14:16:10 +0100

    change class component to functional

--- src/App.js
+++ src/App.js
@@ -1,45 +1,39 @@
-import { Component } from 'react';
-import './App.css';
-import axios from 'axios';
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
 
-class App extends Component {
-    state = {
-        advice: '',
-    };
+const App = () => {
+  const [advice, setAdvice] = useState("");
 
-    componentDidMount() {
-        this.fetchAdvice();
-    }
+  useEffect(() => {
+    fetchAdvice();
+  }, []);
 
-    fetchAdvice = () => {
-        axios
-            .get('https://api.adviceslip.com/advice')
-            .then((response) => {
-                const { advice } = response.data.slip;
-                console.log('quote', advice);
+  const fetchAdvice = () => {
+    axios
+      .get("https://api.adviceslip.com/advice")
+      .then((response) => {
+        const { advice } = response.data.slip;
+        console.log("quote", advice);
 
-                this.setState({ advice });
-            })
-            .catch((error) => {
-                console.log('hm', error);
-            });
-    };
+        setAdvice(advice);
+      })
+      .catch((error) => {
+        console.log("hm", error);
+      });
+  };
 
-    render() {
-        const { advice } = this.state;
-
-        return (
-            <div className='app'>
-                <h1 className='title'>Advice update every 5 seconds</h1>
-                <div className='card'>
-                    <h2 className='heading'>{advice}</h2>
-                    <button className='button' onClick={this.fetchAdvice}>
-                        <span>GIVE ME ADVICE!</span>
-                    </button>
-                </div>
-            </div>
-        );
-    }
-}
+  return (
+    <div className="app">
+      <h1 className="title">Advice update every 5 seconds</h1>
+      <div className="card">
+        <h2 className="heading">{advice}</h2>
+        <button className="button" onClick={fetchAdvice}>
+          <span>GIVE ME ADVICE!</span>
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default App;
